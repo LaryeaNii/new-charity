9c57dbc5fabb0423dceec217700553c356f2b06e
LaryeaNii <zadoklaryea@gmail.com>
2023-08-09 12:16:44 +0000

    volunteer and event

--- src/Volunteer.jsx
+++ src/Volunteer.jsx
@@ -1,7 +1,25 @@
+
+import './Volunteer.css';
+
 const Volunteer = () => {
-    return ( <div>
-        <h1>Volunteer</h1>
-    </div> );
-}
- 
+  return (
+    <div className="larger-container">
+        <div className="volunteer-container">
+      <h2 className="volunteer-heading">Become a Volunteer</h2>
+      <p className="volunteer-description">
+        Join our efforts to make a positive impact in the community. Your contribution matters!
+      </p>
+      <form className="volunteer-form">
+        <input type="text" placeholder="Full Name" />
+        <input type="email" placeholder="Email" />
+        <input type="tel" placeholder="Phone Number" />
+        <textarea placeholder="Tell us why you want to volunteer"></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    </div>
+    
+  );
+};
+
 export default Volunteer;
