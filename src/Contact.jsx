6d77b713a4f8657007539ec8ae2b5cf8b2b265ad
LaryeaNii <zadoklaryea@gmail.com>
2023-08-13 12:46:13 +0000

    volunteer and contact form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,64 +1,69 @@
+import React, { useState, useEffect, useRef } from 'react';
 import './Contact.css';
 import LeafletMap from './LeafMap';
-import { useState, useEffect, useRef  } from 'react';
+import supabase from "./config/supabaseclient";
 import { useLocation } from 'react-router-dom';
 
 const Contact = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const [message, setMessage] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
 
-    const [activeIndex, setActiveIndex] = useState(null);
-     
-    const location = useLocation();
-    const faqSectionRef = useRef(null);
+  const location = useLocation();
+  const faqSectionRef = useRef(null);
 
+  useEffect(() => {
+    if (location.state && location.state.fromContact) {
+      if (faqSectionRef.current) {
+        faqSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location]);
 
-    useEffect(() => {
-        if (location.state && location.state.fromContact) {
-          if (faqSectionRef.current) {
-            faqSectionRef.current.scrollIntoView({ behavior: 'smooth' });
-          }
-        }
-      }, [location]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const faqItems = [
-        {
-            question: "What is your foundation about?",
-            answer: "Our foundation is dedicated to supporting various charitable causes."
-        },
-        {
-            question: "How can I contribute to your projects?",
-            answer: "You can contribute by donating funds or volunteering your time."
-        },
-        {
-            question: "Where are your projects located?",
-            answer: "Our projects are located in various regions across the country."
-        },
-        {
-            question: "Are donations tax-deductible?",
-            answer: "Yes, we are a registered nonprofit organization, and donations are tax-deductible."
-        },
-        {
-            question: "How can I get involved as a volunteer?",
-            answer: "To become a volunteer, you can fill out the volunteer form on our website."
-        },
-        {
-            question: "What types of events do you organize?",
-            answer: "We organize a variety of events, including fundraising galas, community workshops."
-        },
-        {
-            question: "Can I suggest a project for your foundation?",
-            answer: "Yes, we welcome project suggestions from the community. You can submit your idea."
-        },
+    try {
+      const { data, error } = await supabase
+        .from('contact_form')
+        .insert([
+          { message, name, email }
+        ]);
 
-    ];
-    
-    const toggleFaq = (index) => {
-        setActiveIndex(activeIndex === index ? null : index);
-    };
+      if (error) {
+        console.error('Error submitting form:', error);
+        alert(error.message);
+      } else {
+        console.log('Form submitted successfully:', data);
+        setMessage('');
+        setName('');
+        setEmail('');
+        alert('Form submitted successfully!');
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
 
+  const faqItems = [
+    { question: "What is your foundation about?", answer: "Our foundation is dedicated to supporting various charitable causes." },
+    { question: "How can I contribute to your projects?", answer: "You can contribute by donating funds or volunteering your time." },
+    { question: "Where are your projects located?", answer: "Our projects are located in various regions across the country." },
+    { question: "Are donations tax-deductible?", answer: "Yes, we are a registered nonprofit organization, and donations are tax-deductible." },
+    { question: "How can I get involved as a volunteer?", answer: "To become a volunteer, you can fill out the volunteer form on our website." },
+    { question: "What types of events do you organize?", answer: "We organize a variety of events, including fundraising galas, community workshops." },
+    { question: "Can I suggest a project for your foundation?", answer: "Yes, we welcome project suggestions from the community. You can submit your idea." },
+  ];
+  
 
-    return ( 
+  const toggleFaq = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  return (
     <div className="contact-main">
-        <div className="contact-section">
+       <div className="contact-section">
             <div className="contact-title">
             <h1>Get In Touch With Us</h1>
             <p>Please contact us with the information below.</p>
@@ -79,46 +84,38 @@ const Contact = () => {
                 </div>  
         </div>    
         </div>
-        <div className='bottom-contact-section'>
-            <div className="contact-form">
-            <div className='form-title'>
+      <div className='bottom-contact-section'>
+        <div className="contact-form">
+          <div className='form-title'>
             <h2>Send Us A Message</h2>
-            <p>We would love to hear from you. 
-            We try our best to get back to everyone who contacts us.</p>
+            <p>We would love to hear from you. We try our best to get back to everyone who contacts us.</p>
+          </div>
+          <form className='form' onSubmit={handleSubmit}>
+            <textarea name="message" cols="30" rows="10" placeholder='Message' value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+            <div className='input-container'>
+              <input type="text" placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+              <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
-            <form className='form'>             
-                <textarea name="message" cols="30" rows="10" placeholder='Message'></textarea>
-                <div className='input-container'> <input type="text" placeholder='Name'/>
-                <input type="email" placeholder='Email'/></div>
-               
-                <button className='form-button'>Send</button>
-            </form>
+            <button className='form-button' type="submit">Send</button>
+          </form>
+        </div>
+        <LeafletMap />
+      </div>
+      <div ref={faqSectionRef} className="faq-section">
+        <div className="faq-title">
+          <h2>Frequently Asked Questions</h2>
+        </div>
+        <div className="faq-list">
+          {faqItems.map((item, index) => (
+            <div key={index} className={`faq-item ${activeIndex === index ? "active" : ""}`} onClick={() => toggleFaq(index)}>
+              <h3>{item.question}</h3>
+              <p>{item.answer}</p>
             </div>
-        <LeafletMap /> 
-        </div>   
-      
-       <div ref={faqSectionRef} className="faq-section">
-                <div className="faq-title">
-                    <h2>Frequently Asked Questions</h2>
-                </div>
-                <div className="faq-list">
-                    {faqItems.map((item, index) => (
-                        <div
-                            key={index}
-                            className={`faq-item ${activeIndex === index ? "active" : ""}`}
-                            onClick={() => toggleFaq(index)}
-                        >
-                            <h3>{item.question}</h3>
-                            <p>{item.answer}</p>
-                        </div>
-                    ))}
+          ))}
         </div>
-        </div>
-        
-        
+      </div>
+    </div>
+  );
+};
 
-
-    </div>  );
-}
- 
 export default Contact;
--- src/Volunteer.jsx
+++ src/Volunteer.jsx
@@ -1,24 +1,57 @@
-
+import React, { useState } from 'react';
 import './Volunteer.css';
+import supabase from "./config/supabaseclient";
 
 const Volunteer = () => {
+
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [volunteerReasons, setVolunteerReasons] = useState('');
+
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data, error } = await supabase
+        .from('volunteer-form')
+        .insert([
+          { full_name: fullName, email, phone_number: phoneNumber, volunteer_reasons: volunteerReasons }
+        ]);
+
+      if (error) {
+        console.error('Error submitting form:', error);
+        alert(error.message);
+      } else {
+        console.log('Form submitted successfully:', data);
+        setFullName('');
+        setEmail('');
+        setPhoneNumber('');
+        setVolunteerReasons('');
+        alert('Form submitted successfully!');
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
+
   return (
     <div className="larger-container">
-        <div className="volunteer-container">
-      <h2 className="volunteer-heading">Become a Volunteer</h2>
-      <p className="volunteer-description">
-        Join our efforts to make a positive impact in the community. Your contribution matters!
-      </p>
-      <form className="volunteer-form">
-        <input type="text" placeholder="Full Name" />
-        <input type="email" placeholder="Email" />
-        <input type="tel" placeholder="Phone Number" />
-        <textarea placeholder="Tell us why you want to volunteer"></textarea>
-        <button type="submit">Submit</button>
-      </form>
+      <div className="volunteer-container">
+        <h2 className="volunteer-heading">Become a Volunteer</h2>
+        <p className="volunteer-description">
+          Join our efforts to make a positive impact in the community. Your contribution matters!
+        </p>
+        <form className="volunteer-form" onSubmit={handleSubmit}>
+          <input type="text" placeholder="Full Name" value={fullName} onChange={(e) => setFullName(e.target.value)} />
+          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="tel" placeholder="Phone Number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+          <textarea placeholder="Tell us why you want to volunteer" value={volunteerReasons} onChange={(e) => setVolunteerReasons(e.target.value)}></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
-    </div>
-    
   );
 };
 
