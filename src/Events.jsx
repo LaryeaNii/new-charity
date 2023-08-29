b86a087341c42101ab863e6d9b9ec1b2e6faa59d
LaryeaNii <zadoklaryea@gmail.com>
2023-08-29 08:29:11 +0000

    test

--- src/Events.jsx
+++ src/Events.jsx
@@ -59,7 +59,7 @@ const Events = () => {
                 <p>{eventItem.description}</p>
               </div>
               <div className='calender-width'>
-              <div style={{ width: '340px' }}>
+              <div style={{ width: '360px'}}>
                   <Calendar value={new Date(eventItem.the_date)} />
               </div>
                
